6b6476326fbb7e6b478eb1c579bb0452218e41e6
vaspap1790 <vaspap1790@gmail.com>
2024-06-02 11:04:31 +0200

    UserInput Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import Header from "./components/Header.jsx";
+import UserInput from "./components/UserInput.jsx";
 
 const App = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <UserInput />
+    </>
+  );
 };
 
 export default App;
